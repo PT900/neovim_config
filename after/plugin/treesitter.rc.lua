da6a9a0112472fec4870e74668e3dd35cdeede14
Pisanu Boonyaanan <peat2512@hotmail.com>
2023-07-08 14:48:09 +0700

    Enable treesitter settings that I forgot to uncomment cuz of bugged

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -1,35 +1,35 @@
--- require 'nvim-treesitter.install'.compilers = { "zig" }
--- require 'nvim-treesitter.install'.prefer_git = false
--- require 'nvim-treesitter.configs'.setup {
---   -- auto_install = true,
---   highlight = {
---     enable = true,
---     disable = {},
---   },
---   indent = {
---     enable = true,
---     disable = {},
---   },
---   matchup = {
---     enable = true,
---     disable = {},
---   },
---   ensure_installed = {
---     "toml",
---     "fish",
---     "php",
---     "json",
---     "yaml",
---     "swift",
---     "html",
---     "scss",
---     "c",
---     "cpp",
---     "dart",
---     "lua",
---     "vim"
---   },
--- }
---
--- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
--- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+require 'nvim-treesitter.install'.compilers = { "zig" }
+require 'nvim-treesitter.install'.prefer_git = false
+require 'nvim-treesitter.configs'.setup {
+  -- auto_install = true,
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  matchup = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "html",
+    "scss",
+    "c",
+    "cpp",
+    "dart",
+    "lua",
+    "vim"
+  },
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
